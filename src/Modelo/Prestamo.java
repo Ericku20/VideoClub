ddf1fb614f7152a75bf6501227c007cf55ed473a
SofiaHZ <sofiaherrerazuniga@gmail.com>
2023-11-29 22:13:05 -0600

    Clase Prestamo
    atributos y metodos en la clase prestamo

---
+++ src/Modelo/Prestamo.java
@@ -0,0 +1,52 @@
+package Modelo;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Sofia_2
+ */
+public class Prestamo {
+
+    private int numero;
+    private boolean estado;
+    private String fecha;
+    private Clientes cliente;
+    private Peliculas pelicula;
+    private ArrayList<Peliculas> peliculas;
+
+    public Prestamo(int numero, boolean estado, String fecha, Clientes cliente, Peliculas pelicula) {
+        this.numero = numero;
+        this.estado = estado;
+        this.fecha = fecha;
+        this.cliente = cliente;
+        this.pelicula = pelicula;
+        this.peliculas = new ArrayList();
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public Clientes getCliente() {
+        return cliente;
+    }
+
+    public Peliculas getPelicula() {
+        return pelicula;
+    }
+
+    @Override
+    public String toString() {
+        return "Numero: " + numero + " Estado: " + estado + " Fecha: " + fecha + " Cliente: " + cliente + " Pelicula: " + pelicula;
+    }
+
+}
