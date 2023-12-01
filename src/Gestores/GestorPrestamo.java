22e90be61e19f9aebdcec161200911d0c7d35864
ThailynParrales <shailyn04parrales@gmail.com>
2023-12-01 15:35:44 -0600

    Se creo la clase gestorPrestamo
    Contiene un CRUD de los que sucede en prestamo

--- src/Gestores/GestorPrestamo.java
+++ src/Gestores/GestorPrestamo.java
@@ -4,10 +4,56 @@
  */
 package Gestores;
 
+import Modelo.Clientes;
+import Modelo.Peliculas;
+import Modelo.Prestamo;
+import java.util.ArrayList;
+
 /**
  *
  * @author uno
  */
 public class GestorPrestamo {
+    private ArrayList<Prestamo> prestamosList;
+    Prestamo prestamo;
     
+
+    public GestorPrestamo() {
+        this.prestamosList = new ArrayList<>();
+    }
+
+    public Prestamo getPrestamo() {
+        return prestamo;
+    }
+
+    public ArrayList<Prestamo> getPrestamosList() {
+        return prestamosList;
+    }
+
+    public void agregarPrestamo(Prestamo prestamo) {
+        prestamosList.add(prestamo);
+    }
+
+    public Prestamo obtenerProximoPrestamo() {
+        if (!prestamosList.isEmpty()) {
+            return prestamosList.remove(0);
+        }
+        return null;
+    }
+    
+    public void eliminarPrestamo(Prestamo prestamo) {
+        prestamosList.remove(prestamo);
+    }
+
+    public void modificarPrestamo(Prestamo prestamo, boolean nuevoEstado, String nuevaFecha,
+            Clientes nuevoCliente, Peliculas nuevaPelicula) {
+        if (prestamosList.contains(prestamo)) {
+            prestamo.setEstado(nuevoEstado);
+            prestamo.setFecha(nuevaFecha);
+            prestamo.setCliente(nuevoCliente);
+            prestamo.setPelicula(nuevaPelicula);
+        } else {
+            System.out.println("Error: El préstamo no existe en la lista.");
+        }
+    }
 }
