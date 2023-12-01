c8fbbd87a28c7ef6bbacb511c690cda5bd318ed4
SofiaHZ <sofiaherrerazuniga@gmail.com>
2023-12-01 15:44:51 -0600

    Gestor Cliente
    Se crea la clase del gestor cliente

--- src/Gestores/GestorCliente.java
+++ src/Gestores/GestorCliente.java
@@ -4,10 +4,48 @@
  */
 package Gestores;
 
+import Modelo.Clientes;
+import java.util.HashSet;
+
 /**
  *
  * @author uno
  */
 public class GestorCliente {
+
+    private HashSet<Clientes> cliente;
+
+    public GestorCliente(HashSet<Clientes> cliente) {
+        this.cliente = new HashSet<>();
+    }
+
+    public HashSet<Clientes> getClientes() {
+        return cliente;
+    }
+
+    public void agregarCliente(Clientes c) {
+        cliente.add(c);
+    }
+
+    public void eliminarCliente(Clientes c, GestorPrestamo prestamo) {
+        
+    }
     
+    public Clientes buscarCliente(int cedula){
+         for (Clientes c : cliente) {
+            if (c.getCedula()== cedula) {
+                return c;
+            }
+        }
+        return null;
+    } 
+
+//    private boolean clienteEnUso(Clientes c, GestorPrestamo prestamo) {
+//        for (Prestamo prestamo : gestorPrestamo.getPrestamos()) {
+//            if (prestamo.getCliente().equals(cliente)) {
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
 }
